177f8b39512c7b440ab740d677b9dae8df464cfc
mcqtip22 <bmcewen609@gmail.com>
2020-03-30 10:31:02 -0400

    updates listener

--- index.js
+++ index.js
@@ -11,6 +11,6 @@ app.get('/', function(req, res) {
 	res.sendFile('index.html');
 });
 
-app.listen(3000, function() {
-	console.log('App running on port 3000');
+app.listen(process.env.PORT || 3000, function() {
+	console.log('Express server listening on port %d in %s mode', this.address().port, app.settings.env);
 });
