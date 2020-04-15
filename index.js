4d864b68f92510efd587e0e36596e6895478cfe3
mcqtip22 <bmcewen609@gmail.com>
2020-04-15 15:01:50 -0400

    made cards/added electric hive

--- index.js
+++ index.js
@@ -11,14 +11,6 @@ app.get('/', function(req, res) {
 	res.sendFile('index.html');
 });
 
-app.get('/pomodoro', function(req, res) {
-	res.status(301).redirect('https://limitless-atoll-83895.herokuapp.com/');
-});
-
-app.get('/passgen', function(req, res) {
-	res.status(301).redirect('https://fast-atoll-46610.herokuapp.com/');
-});
-
 app.listen(process.env.PORT || 3000, function() {
 	console.log('Express server listening on port %d in %s mode', this.address().port, app.settings.env);
 });
