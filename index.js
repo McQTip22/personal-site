7087a824661a4fffaacc8635d11aa3c31692a207
mcqtip22 <bmcewen609@gmail.com>
2020-04-17 12:26:05 -0400

    added/rearanged project

--- index.js
+++ index.js
@@ -1,9 +1,6 @@
 var express = require('express'),
 	app = express();
-bodyParser = require('body-parser');
 
-app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({ extended: true }));
 app.use(express.static(__dirname + '/public'));
 app.use(express.static(__dirname + '/views'));
 
